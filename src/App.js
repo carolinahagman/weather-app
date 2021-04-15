0ba5d9f6aff314062d5212c668f109edd439d065
carolinahagman <hagmancarolina@gmail.com>
2021-04-15 13:44:33 +0200

    Add start of project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import Title from "./components/title";
+import Form from "./components/input-form";
+
+function App() {
+  return (
+    <div className="text-center mt-4">
+      <Form></Form>
+      <div className=""></div>
+   <Title>check the weather</Title>
+   
+    
+    </div>
+  );
+}
+
+export default App;
